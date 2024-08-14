b1aec4c4eb280dfb5baa402767902f454f4096e7
matheus4798 <146399118+matheus4798@users.noreply.github.com>
2024-08-14 00:24:56 +0000

    aula1

---
+++ aula1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program{
+    public static void Main(string[] args){
+        //Cadastrar os dados de pessoa
+        Console.WriteLine("Cadastro de pessoa !");
+        Console.Write ("Nome: ");
+        string nomePessoa = Console.ReadLine();
+        
+        Console.Write("Idade: ");
+        int idadePessoa = int.Parse(Console.ReadLine());
+
+        Pessoa pessoa1 = new Pessoa(nomePessoa, idadePessoa);
+
+        Console.WriteLine(pessoa1.ToString());
+
+    }
+
+    
+}
